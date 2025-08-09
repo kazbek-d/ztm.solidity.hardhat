54f24b7b91a7fd72c81f9a6f0f6692f3e79b644d
Anzhela DZARASOVA <angelas82@list.ru>
2025-08-09 10:46:31 +0200

    Used contract.connect function to specify sender

--- test/ERC20.ts
+++ test/ERC20.ts
@@ -3,22 +3,29 @@ import { expect } from "chai";
 
 describe("ERC20", function () {
     it("transfers tokens correctly", async function () {
-        const [bob, alice] = await ethers.getSigners();
+        const [alice, bob] = await ethers.getSigners();
 
         const ERC20 = await ethers.getContractFactory("ERC20Mock");
-        const erc20Token = (await ERC20.deploy("Name", "SYM", 18)).connect(alice);
+        const erc20Token = (await ERC20.deploy("Name", "SYM", 18));
 
         await erc20Token.mint(alice.address, 300);
 
-        console.log("Alice balance before transfer is", await erc20Token.balancesOf(alice.address));
-        console.log("Bob balance before transfer is", await erc20Token.balancesOf(bob.address));
+        let aliceBalance = await erc20Token.balancesOf(alice.address);
+        let bobBalance = await erc20Token.balancesOf(bob.address);
+
+        console.log("Alice balance before transfer is", aliceBalance);
+        console.log("Bob balance before transfer is", bobBalance);
 
         await erc20Token.transfer(bob.address, 100);
-
-        const aliceBalance = await erc20Token.balancesOf(alice.address);
-        const bobBalance = await erc20Token.balancesOf(bob.address);
-
+        aliceBalance = await erc20Token.balancesOf(alice.address);
+        bobBalance = await erc20Token.balancesOf(bob.address);
         expect(aliceBalance).to.equals(200);
         expect(bobBalance).to.equals(100);
+
+        await erc20Token.connect(bob).transfer(alice.address, 50);
+        aliceBalance = await erc20Token.balancesOf(alice.address);
+        bobBalance = await erc20Token.balancesOf(bob.address);
+        expect(aliceBalance).to.equals(250);
+        expect(bobBalance).to.equals(50);
     });
 });
