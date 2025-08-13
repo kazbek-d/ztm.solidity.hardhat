7400fe4d479b8682cf8defb6b482f90ea64979bc
Anzhela DZARASOVA <angelas82@list.ru>
2025-08-13 08:31:48 +0200

    Added beforeEach

--- test/ERC20.ts
+++ test/ERC20.ts
@@ -1,15 +1,25 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
+import type { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { ContractTransactionResponse } from "ethers";
+import { ERC20Mock } from "../typechain-types";
 
 describe("ERC20", function () {
-    it("transfers tokens correctly", async function () {
-        const [alice, bob] = await ethers.getSigners();
+    let alice: HardhatEthersSigner, bob: HardhatEthersSigner;
+    let erc20Token: ERC20Mock & {
+        deploymentTransaction(): ContractTransactionResponse;
+    };
+
+    this.beforeEach(async function () {
+        [alice, bob] = await ethers.getSigners();
 
         const ERC20 = await ethers.getContractFactory("ERC20Mock");
-        const erc20Token = (await ERC20.deploy("Name", "SYM", 18));
+        erc20Token = await ERC20.deploy("Name", "SYM", 18);
 
         await erc20Token.mint(alice.address, 300);
+    });
 
+    it("transfers tokens correctly", async function () {
         let aliceBalance = await erc20Token.balancesOf(alice.address);
         let bobBalance = await erc20Token.balancesOf(bob.address);
 
@@ -30,25 +40,11 @@ describe("ERC20", function () {
     });
 
     it("should revert if sender insuffisient balance", async function () {
-        const [alice, bob] = await ethers.getSigners();
-
-        const ERC20 = await ethers.getContractFactory("ERC20Mock");
-        const erc20Token = (await ERC20.deploy("Name", "SYM", 18));
-
-        await erc20Token.mint(alice.address, 300);
-
         await expect(erc20Token.transfer(bob.address, 400)).to.be.reverted;
         await expect(erc20Token.transfer(bob.address, 400)).to.be.revertedWith("ERC20: Insufficient sender balance");
     })
 
     it("should emit Transfer event on transfers", async function () {
-        const [alice, bob] = await ethers.getSigners();
-
-        const ERC20 = await ethers.getContractFactory("ERC20Mock");
-        const erc20Token = (await ERC20.deploy("Name", "SYM", 18));
-
-        await erc20Token.mint(alice.address, 300);
-
         await expect(erc20Token.transfer(bob.address, 200))
             .to.emit(erc20Token, "Transfer")
             .withArgs(alice.address, bob.address, 200);
