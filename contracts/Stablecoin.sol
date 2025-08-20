3a472ee08d7376ea4dbc737133d60661baf3c4cb
Anzhela DZARASOVA <angelas82@list.ru>
2025-08-20 07:31:05 +0200

    Added Stablecoin and DepositorCoin contracts

---
+++ contracts/Stablecoin.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {console} from "hardhat/console.sol";
+import {ERC20} from "./ERC20.sol";
+import {DepositorCoin} from "./DepositorCoin.sol";
+
+contract Stablecoin is ERC20 {
+    DepositorCoin public depositorCoin;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {}
+}
